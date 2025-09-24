dd0514cb32244b74a01dcb5480d5c6d72a616b87
703337 <da703337@gmail.com>
2025-09-25 11:23:56 +1200

    More cards!

--- Assets/Scripts/DrawCard.cs
+++ Assets/Scripts/DrawCard.cs
@@ -9,6 +9,22 @@ public class DrawCard : MonoBehaviour
     // Initial References
     [SerializeField] GameObject Red2;
     [SerializeField] GameObject Black2;
+    [SerializeField] GameObject Red3;
+    [SerializeField] GameObject Black3;
+    [SerializeField] GameObject Red4;
+    [SerializeField] GameObject Black4;
+    [SerializeField] GameObject Red5;
+    [SerializeField] GameObject Black5;
+    [SerializeField] GameObject Red6;
+    [SerializeField] GameObject Black6;
+    [SerializeField] GameObject Red7;
+    [SerializeField] GameObject Black7;
+    [SerializeField] GameObject Red8;
+    [SerializeField] GameObject Black8;
+    [SerializeField] GameObject Red9;
+    [SerializeField] GameObject Black9;
+    [SerializeField] GameObject Red10;
+    [SerializeField] GameObject Black10;
     [SerializeField] GameObject RedJoker;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -42,7 +58,7 @@ public class DrawCard : MonoBehaviour
     public void DrawCards()
     {
         GameObject pickedCard;
-        int pickedCardInt = UnityEngine.Random.Range(0, 54);
+        int pickedCardInt = UnityEngine.Random.Range(0, 17);
         switch (pickedCardInt)
         {
             case 0:
@@ -50,6 +66,54 @@ public class DrawCard : MonoBehaviour
                 break;
             case 1:
                 pickedCard = Black2;
+                break;
+            case 2:
+                pickedCard = Red3;
+                break;
+            case 3:
+                pickedCard = Black3;
+                break;
+            case 4:
+                pickedCard = Red4;
+                break;
+            case 5:
+                pickedCard = Black4;
+                break;
+            case 6:
+                pickedCard = Red5;
+                break;
+            case 7:
+                pickedCard = Black5;
+                break;
+            case 8:
+                pickedCard = Red6;
+                break;
+            case 9:
+                pickedCard = Black6;
+                break;
+            case 10:
+                pickedCard = Red7;
+                break;
+            case 11:
+                pickedCard = Black7;
+                break;
+            case 12:
+                pickedCard = Red8;
+                break;
+            case 13:
+                pickedCard = Black8;
+                break;
+            case 14:
+                pickedCard = Red9;
+                break;
+            case 15:
+                pickedCard = Black9;
+                break;
+            case 16:
+                pickedCard = Red10;
+                break;
+            case 17:
+                pickedCard = Black10;
                 break;
             default:
                 pickedCard = RedJoker;
